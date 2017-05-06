6eddd9b6ecc14b17c4a74f963b94d4d957cd718b
John P Bloch <johnpbloch@gmail.com>
2017-05-06 15:46:30 -0500

    Add some helper functions

---
+++ extra/functions/helpers.php
@@ -0,0 +1,90 @@
+<?php
+
+if ( ! function_exists( 'value' ) ) {
+	/**
+	 * Return the default value of the given value.
+	 *
+	 * @param  mixed $value
+	 *
+	 * @return mixed
+	 */
+	function value( $value ) {
+		return $value instanceof Closure ? $value() : $value;
+	}
+}
+
+if ( ! function_exists( 'str_starts_with' ) ) {
+	/**
+	 * Determine if a given string starts with a given substring.
+	 *
+	 * @param  string       $haystack
+	 * @param  string|array $needles
+	 *
+	 * @return bool
+	 */
+	function str_starts_with( $haystack, $needles ) {
+		foreach ( (array) $needles as $needle ) {
+			if ( $needle != '' && substr( $haystack, 0, strlen( $needle ) ) === (string) $needle ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+if ( ! function_exists( 'str_ends_with' ) ) {
+	/**
+	 * Determine if a given string ends with a given substring.
+	 *
+	 * @param  string       $haystack
+	 * @param  string|array $needles
+	 *
+	 * @return bool
+	 */
+	function str_ends_with( $haystack, $needles ) {
+		foreach ( (array) $needles as $needle ) {
+			if ( substr( $haystack, - strlen( $needle ) ) === (string) $needle ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+if ( ! function_exists( 'env' ) ) {
+	/**
+	 * Gets the value of an environment variable.
+	 *
+	 * @param  string $key
+	 * @param  mixed  $default
+	 *
+	 * @return mixed
+	 */
+	function env( $key, $default = null ) {
+		$value = getenv( $key );
+		if ( $value === false ) {
+			return value( $default );
+		}
+		switch ( strtolower( $value ) ) {
+			case 'true':
+			case '(true)':
+				return true;
+			case 'false':
+			case '(false)':
+				return false;
+			case 'empty':
+			case '(empty)':
+				return '';
+			case 'null':
+			case '(null)':
+				return null;
+		}
+		if ( strlen( $value ) > 1 && str_starts_with( $value, '"' ) && str_ends_with( $value, '"' ) ) {
+			return substr( $value, 1, - 1 );
+		}
+
+		return $value;
+	}
+}
